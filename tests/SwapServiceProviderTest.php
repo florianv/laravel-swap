7c18d50f0f670fdea7691ab35022f53fd61f74c5
florianv <voutzinos.florian@gmail.com>
2016-09-16 03:07:41 +0200

    Added service provider, tests

--- tests/SwapServiceProviderTest.php
+++ tests/SwapServiceProviderTest.php
@@ -9,124 +9,121 @@
  * file that was distributed with this source code.
  */
 
-namespace Florianv\LaravelSwap\Tests;
+namespace Swap\Laravel\Tests;
 
+use Exchanger\ExchangeRate;
 use GrahamCampbell\TestBench\Traits\ServiceProviderTestCaseTrait;
-use Ivory\HttpAdapter\CurlHttpAdapter;
+use Exchanger\Service\Chain;
+use Http\Discovery\HttpClientDiscovery;
+use Http\Discovery\MessageFactoryDiscovery;
+use Psr\Cache\CacheItemPoolInterface;
+use Swap\Swap;
 
 class SwapServiceProviderTest extends AbstractTestCase
 {
     use ServiceProviderTestCaseTrait;
 
-    public function testDefauktHttpAdapter()
-    {
-        $this->assertInstanceOf('Ivory\HttpAdapter\FileGetContentsHttpAdapter', $this->app['swap.http_adapter']);
-    }
-
-    public function testCustomHttpAdapter()
-    {
-        $adapter = new CurlHttpAdapter();
-        $this->app->singleton('swap.http_adapter_curl', function () use ($adapter) {
-            return $adapter;
-        });
-
-        $this->app->config->set('swap.http_adapter', 'swap.http_adapter_curl');
-        $this->assertSame($adapter, $this->app['swap.http_adapter']);
-    }
-
-    public function testDefaultCache()
-    {
-        $this->assertNull($this->app['swap.cache']);
-    }
-
-    /**
-     * @expectedException \RuntimeException
-     * @expectedExceptionMessage Unknown cache type "unknown".
-     */
-    public function testUnknownCache()
-    {
-        $this->app->config->set('swap.cache', [
-            'type' => 'unknown',
-        ]);
-        $this->app['swap.cache'];
-    }
-
     /**
      * @expectedException \InvalidArgumentException
-     * @expectedExceptionMessage Cache store [not_found] is not defined
+     * @expectedExceptionMessage The "access_key" option must be provided.
      */
-    public function testIlluminateCacheStoreNotFound()
+    public function testMissingServiceConfig()
     {
-        $this->app->config->set('swap.cache', [
-            'type' => 'illuminate',
-            'store' => 'not_found',
-            'ttl' => 60,
-        ]);
-        $this->app['swap.cache'];
-    }
-
-    public function testIlluminateCache()
-    {
-        $this->app->config->set('cache', [
-            'stores' => [
-                'array' => [
-                    'driver' => 'array',
-                ],
-            ],
+        $this->app->config->set('swap.services', [
+            'currencylayer' => true,
         ]);
 
-        $this->app->config->set('swap.cache', [
-            'type' => 'illuminate',
-            'store' => 'array',
-            'ttl' => 60,
-        ]);
-
-        $this->assertInstanceOf('Swap\Cache\IlluminateCache', $this->app['swap.cache']);
-    }
-
-    public function testDefaultProvider()
-    {
-        $this->assertInstanceOf('Swap\Provider\ChainProvider', $this->app['swap.provider']);
-    }
-
-    public function testAllProviders()
-    {
-        $this->app->config->set('swap.providers', [
-            'yahoo_finance' => true,
-            'google_finance' => true,
-            'european_central_bank' => true,
-            'national_bank_of_romania' => true,
-            'webservicex' => true,
-            'open_exchange_rates' => [
-                'app_id' => 'foo',
-                'enterprise' => true,
-            ],
-            'xignite' => [
-                'token' => 'bar',
-            ],
-            'central_bank_of_republic_turkey' => true,
-            'central_bank_of_czech_republic' => true,
-        ]);
-
-        $this->assertInstanceOf('Swap\Provider\ChainProvider', $this->app['swap.provider']);
+        $this->app['swap'];
     }
 
     /**
      * @expectedException \RuntimeException
-     * @expectedExceptionMessage Unknown provider with name "bankster"
+     * @expectedExceptionMessage The service "unknown" does not exist.
      */
-    public function testUnknownProvider()
+    public function testUnknownService()
     {
-        $this->app->config->set('swap.providers', [
-            'bankster' => true,
+        $this->app->config->set('swap.services', [
+            'unknown' => true,
         ]);
 
-        $this->app['swap.provider'];
+        $this->app['swap'];
+    }
+
+    public function testEmptyServices()
+    {
+        $this->app['swap'];
+    }
+
+    public function testAllServices()
+    {
+        $this->app->config->set('swap.services', [
+            'central_bank_of_czech_republic' => true,
+            'central_bank_of_republic_turkey' => true,
+            'currencylayer' => ['access_key' => 'secret', 'enterprise' => false],
+            'european_central_bank' => true,
+            'fixer' => true,
+            'google' => true,
+            'national_bank_of_romania' => true,
+            'open_exchange_rates' => ['app_id' => 'secret', 'enterprise' => false],
+            'array' => [['EUR/USD' => new ExchangeRate('1.5')]],
+            'webservicex' => true,
+            'xignite' => ['token' => 'token'],
+            'yahoo' => true
+        ]);
+
+        $this->assertInstanceOf(Chain::class, $this->app['swap.chain']);
     }
 
     public function testSwapIsInjectable()
     {
-        $this->assertIsInjectable('Swap\SwapInterface');
-        $this->assertIsInjectable('Swap\Swap');
+        $this->assertIsInjectable(Swap::class);
+    }
+
+    public function testOptions()
+    {
+        $this->app->config->set('swap.options', ['cache_ttl' => 60]);
+        $this->assertInstanceOf(Swap::class, $this->app['swap']);
+    }
+
+    public function testCustomHttpClient()
+    {
+        $client = HttpClientDiscovery::find();
+
+        $this->app->singleton('http_client_custom', function () use ($client) {
+            return $client;
+        });
+
+        $this->app->config->set('swap.http_client', 'http_client_custom');
+
+        $this->assertSame($client, $this->app['swap.http_client']);
+        $this->assertInstanceOf(Swap::class, $this->app['swap']);
+    }
+
+    public function testCustomRequestFactory()
+    {
+        $requestFactory = MessageFactoryDiscovery::find();
+
+        $this->app->singleton('request_factory_custom', function () use ($requestFactory) {
+            return $requestFactory;
+        });
+
+        $this->app->config->set('swap.request_factory', 'request_factory_custom');
+
+        $this->assertSame($requestFactory, $this->app['swap.request_factory']);
+        $this->assertInstanceOf(Swap::class, $this->app['swap']);
+    }
+
+    public function testCustomCacheItemPool()
+    {
+        $cacheItemPool = $this->getMock(CacheItemPoolInterface::class);
+
+        $this->app->singleton('cache_item_pool_custom', function () use ($cacheItemPool) {
+            return $cacheItemPool;
+        });
+
+        $this->app->config->set('swap.cache_item_pool', 'cache_item_pool_custom');
+
+        $this->assertSame($cacheItemPool, $this->app['swap.cache_item_pool']);
+        $this->assertInstanceOf(Swap::class, $this->app['swap']);
     }
 }
